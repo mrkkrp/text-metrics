9e7cb598a763aa958a977e457d3be1adb1586633
mrkkrp <markkarpov@opmbx.org>
2016-09-03 23:47:27 +0300

    Add benchmarks

--- bench/Main.hs
+++ bench/Main.hs
@@ -32,5 +32,31 @@
 
 module Main (main) where
 
+import Control.DeepSeq
+import Criterion.Main
+import Data.Text (Text)
+import Data.Text.Metrics
+import qualified Data.Text as T
+
 main :: IO ()
-main = return ()
+main = defaultMain
+  [ btmetric "levenshtein"            levenshtein
+  , btmetric "levenshteinNorm"        levenshteinNorm
+  , btmetric "damerauLevenshtein"     damerauLevenshtein
+  , btmetric "damerauLevenshteinNorm" damerauLevenshteinNorm
+  , btmetric "hamming"                hamming ]
+
+-- | Produce benchmark group to test
+
+btmetric :: NFData a => String -> (Text -> Text -> a) -> Benchmark
+btmetric name f = bgroup name (bs <$> stdSeries)
+  where
+    bs n = env (return (testData n, testData n)) (bench (show n) . nf (uncurry f))
+
+-- | The series of lengths to try with every function as part of 'btmetric'.
+
+stdSeries :: [Int]
+stdSeries = [5,10,20,40,80,160]
+
+testData :: Int -> Text
+testData n = T.pack . take n . drop (n `mod` 4) . cycle $ ['a'..'z']
