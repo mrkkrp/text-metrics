afdfab233b46c4eeec9a78c693415f1669ef338e
mrkkrp <markkarpov@opmbx.org>
2016-10-08 12:34:06 +0300

    Implement Jaro and Jaro-Winkler distances

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,8 @@
 * Made the `levenshtein`, `levenshteinNorm`, `damerauLevenshtein`, and
   `demerauLevenshtein` more efficient.
 
+* Added `jaro` and `jaroWinkler` functions.
+
 ## Text Metrics 0.1.0
 
 * Initial release.
--- cbits/text_metrics.h
+++ cbits/text_metrics.h
@@ -37,6 +37,11 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+#define VLEN_MAX 255 /* Up to this length we use alloca. */
+
 /* Levenshein variants */
 
 unsigned int tmetrics_levenshtein (unsigned int, uint16_t *, unsigned int, uint16_t *);
@@ -45,5 +50,7 @@ unsigned int tmetrics_damerau_levenshtein (unsigned int, uint16_t *, unsigned in
 /* Other */
 
 unsigned int tmetrics_hamming (unsigned int, uint16_t *, uint16_t *);
+void tmetrics_jaro (unsigned int *, unsigned int *, unsigned int, uint16_t *, unsigned int, uint16_t *);
+unsigned int tmetrics_common_prefix (unsigned int, uint16_t *, unsigned int, uint16_t *);
 
 #endif /* TEXT_METRICS_H */
